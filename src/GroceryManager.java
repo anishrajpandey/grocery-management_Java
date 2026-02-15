64c6c77e8b098e66c9ab5f6b1f4e06ea568b99f9
Anish <anishpandey021@gmail.com>
2026-02-15 12:31:32 -0600

    cleaning boilerplate

--- src/GroceryManager.java
+++ src/GroceryManager.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 
 public class GroceryManager {
+    public GroceryManager() {
+
+    }
+
     public static void main(String[] args) {
         // Task: Parallel Array Architecture
         String[] itemNames = new String[10];
         double[] itemPrices = new double[10];
         int[] itemStocks = new int[10];
 
-        System.out.println("Grocery Management System Initialized.");
     }
 
     /**
@@ -15,7 +18,6 @@ public class GroceryManager {
      * Branch: feature-display
      */
     public static void printInventory(String[] names, double[] prices, int[] stocks) {
-        // TODO: Teammate A implements loop and null-check logic here [cite: 21, 22]
     }
 
     /**
