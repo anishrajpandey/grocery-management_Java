90edc4b9089a51fe0a0c30709397764ddf2d8977
Anish <anishpandey021@gmail.com>
2026-02-18 23:29:41 -0600

    decorative fixes

--- src/GroceryManager.java
+++ src/GroceryManager.java
@@ -43,16 +43,16 @@ public class GroceryManager {
 
     public static void printInventory(String[] names, double[] prices, int[] stocks) {
 
-    for (int i = 0; i < names.length; i++) {
+        for (int i = 0; i < names.length; i++) {
 
-        if (names[i] != null) {
-            System.out.println("Item: " + names[i] +
-                               " | Price: $" + prices[i] +
-                               " | Stock: " + stocks[i]);
+            if (names[i] != null) {
+                System.out.println("Item: " + names[i] +
+                        " | Price: $" + prices[i] +
+                        " | Stock: " + stocks[i]);
+            }
         }
     }
-}
-    
+
     /**
      * Task 2: Searches for an item by name and updates its stock quantity.
      * Branch: feature-restock
